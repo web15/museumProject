4ef91f024b0dabf562cec1ea33c49693a8529289
Charles Marks <clmarks15@gmail.com>
2019-01-04 20:02:12 -0500

    ready for final review

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //alert("working");
 
+$(function ()   {
+    $(document).scroll(function(){
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
